d68508d6c02af2920bbab10b90211f3c4421d41a
How2Compute <howtocompute123@gmail.com>
2017-06-27 18:42:48 +0000

    Fixed index code

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -3,7 +3,10 @@ CREATE DATABASE url_short;
 
 /* Use \c url_short to connect to that database
    Create the urls table TODO make id auto increment & a primary key */
-CREATE TABLE urls(id SERIAL PRIMARY KEY, short_code CHAR(50), full_url CHAR(256));
+CREATE TABLE urls(id SERIAL PRIMARY KEY, short_code CHAR(50) UNIQUE, full_url CHAR(256));
+
+/* Optimize short_code field for indexing */
+CREATE INDEX short_code ON urls(short_code)
 
 /* Insert a test record */
 INSERT INTO urls (id, short_code, full_url) VALUES (1, 'TEST', 'https://google.com');
