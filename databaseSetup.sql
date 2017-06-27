f08f4d54caf5fa53f88dba37a529aa6f3e1432e6
How2Compute <howtocompute123@gmail.com>
2017-06-27 05:10:06 +0000

    Made id field auto-increment

--- databaseSetup.sql
+++ databaseSetup.sql
@@ -3,7 +3,7 @@ CREATE DATABASE url_short;
 
 /* Use \c url_short to connect to that database
    Create the urls table TODO make id auto increment & a primary key */
-CREATE TABLE urls(id INT PRIMARY KEY, short_code CHAR(50), full_url CHAR(256))
+CREATE TABLE urls(id SERIAL PRIMARY KEY, short_code CHAR(50), full_url CHAR(256));
 
 /* Insert a test record */
 INSERT INTO urls (id, short_code, full_url) VALUES (1, 'TEST', 'https://google.com');
